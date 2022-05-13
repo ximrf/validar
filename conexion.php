a3ac06c06e3f4f2735046a35c8e37a3e9c9515c3
ximrf <pinkfull_xime@hotmail.com>
2022-05-12 19:18:48 -0500

    archivos iniciales

---
+++ conexion.php
@@ -0,0 +1,3 @@
+<?
+
+	$db = pg_connect("host=localhost port 5432 dbname=formulario user=ximenarf pasward=hola1234");
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+	<head>
+		<meta charset= "utf-8">
+		<title>Formulario</title>
+	</head>
+	<body>
+		<script src="js/jquery-3.6.0.js"></script>
+		<script src="js/jquery.validation.min.js"></script>
+		<script src="js/ajax.js"></script
+
+
+		<h5>Por favor, ingrese los datos en el siguiente formulario:</h5>
+		<form id="formulario">
+			<br><label for="nombre">Nombre: </label>
+			<br><input type="text" id="nombre" name="nombre">
+			<br><label for="apaterno">Apellido Paterno </label>
+			<br><input type"text" id="apaterno" name=apaterno>
+			<br><label for="amaterno">Apellido Materno: </label
+			<br><input type="text" id="amaterno" name="amaterno">
+			<button type="submit">Enviar</Button>
+		</form>
+	</body>
+</html>
+
